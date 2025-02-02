d17ff85cba6b2a0b8a7f67836ed3954209d551ab
Snepx <153798266+Snepx@users.noreply.github.com>
2025-02-02 08:46:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Javascript para adicionar interatividade (exemplo)
+const mangaSection = document.getElementById('manga');
+
+mangaSection.addEventListener('click', () => {
+  alert('Você clicou na seção sobre a manga!');
+});
